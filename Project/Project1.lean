cc845491f9f9ebec04ecdf630357dd1a78543f6b
Christopher Loura <cmloura@users.campus>
2023-04-04 15:50:07 -0500

    testing what we have so far

--- Project/Project1.lean
+++ Project/Project1.lean
@@ -9,3 +9,15 @@ def DownwardsClosed (d : Set ℚ) : Prop :=
   ∀ r r' : ℚ, ((r'∈ d) ∧ (r < r')) → (r ∈ d)  
 
   -- Set.univ (for dedekind cut instead of T)
+
+def DedekindCut(d : Set ℚ) : Prop :=
+  DownwardsClosed(d) ∧ d ≠ ∅ ∧ d ≠ Set.univ
+
+def Real : Type :=
+  {x : Set ℚ // DedekindCut(x)}
+
+def IsUpperBound(S : Set) (n : Real) : Prop :=
+  ∀ x : Real, (x ∈ S) → (n >= x)
+
+def IsLUB (S : Set) (n : Real) : Prop :=
+  ∀ x : Real, ((IsUpperBound S x) → n <= x) ∧ IsUpperBound S n
